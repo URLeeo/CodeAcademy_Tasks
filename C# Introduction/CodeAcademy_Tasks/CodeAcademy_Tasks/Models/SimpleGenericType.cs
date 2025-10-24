b92e417edfa61b95cb17f724f379ab160fc20363
URLeeo <mammadzadaaslan05@gmail.com>
2025-10-24 19:47:14 +0400

    Added new task about generics

---
+++ C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Models/SimpleGenericType.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeAcademy_Tasks.Models;
+
+public  class SimpleGenericType<T>
+{
+    public T RandomValue { get; set; }
+    public SimpleGenericType(T randommValue)
+    {
+        RandomValue = randommValue;
+    }
+
+    public void Print(T value)
+    {
+        Console.WriteLine();
+    }
+}
--- C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Program.cs
+++ C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Program.cs
@@ -473,3 +473,20 @@ using System.Security.Cryptography.X509Certificates;
 //Kelvin k = c;
 //Console.WriteLine(k.Degree);
 #endregion
+
+#region Generic Type Usage
+//Pair<int, string> pair1 = new Pair<int, string>(1, "One");
+//pair1.Display();
+
+//Pair<string, string> pair2 = new Pair<string, string>("Hello", "World");
+//pair2.Display();
+
+//SimpleGenericType<int> intExample = new(52);
+//intExample.Print(intExample.RandomValue);
+
+//SimpleGenericType<string> stringExample = new("Hello, Generics");
+//stringExample.Print(stringExample.RandomValue);
+
+//SimpleGenericType<double> doubleExample = new(3.14);
+//doubleExample.Print(doubleExample.RandomValue);
+#endregion
