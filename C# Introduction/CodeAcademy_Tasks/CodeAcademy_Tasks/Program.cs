ec9507b21633aa6e3e1fe8e73653a38b8dbbde6a
URLeeo <mammadzadaaslan05@gmail.com>
2025-10-28 19:08:29 +0400

    Added New Tasks

--- C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Program.cs
+++ C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Program.cs
@@ -236,6 +236,7 @@ using CodeAcademy_Tasks.Helpers;
 using CodeAcademy_Tasks.Implementations;
 using CodeAcademy_Tasks.Models;
 using System;
+using System.Collections;
 using System.Diagnostics.Tracing;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
@@ -489,4 +490,53 @@ using System.Security.Cryptography.X509Certificates;
 
 //SimpleGenericType<double> doubleExample = new(3.14);
 //doubleExample.Print(doubleExample.RandomValue);
+#endregion
+
+#region Collections Usage
+#region List
+//List<string> names = new List<string> { "Aslan", "Leyla", "Aslan" };
+//names.Add("Aslan");
+
+//foreach (var name in names)
+//    Console.WriteLine(name);
+
+//Console.WriteLine($"First name: {names[0]}");
+#endregion
+
+#region Hashset
+//HashSet<int> numbers = new HashSet<int> { 1, 2, 3, 3 };
+//numbers.Add(4);
+
+//foreach (var n in numbers)
+//    Console.WriteLine(n);
+
+//Console.WriteLine(numbers.Contains(3));
+#endregion
+
+#region SortedList
+//SortedList<int, string> sortedList = new();
+
+//sortedList.Add(3, "C");
+//sortedList.Add(1, "A");
+//sortedList.Add(2, "B");
+
+//foreach (var kvp in sortedList)
+//    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+#endregion
+
+#region Hashtable
+//Hashtable table = new Hashtable();
+
+//table.Add(1, "A");
+//table.Add(2, "B");
+//table[3] = "C";
+
+//foreach (DictionaryEntry entry in table)
+//{
+//    Console.WriteLine($"{entry.Key}: {entry.Value}");
+//}
+
+//if (table.ContainsKey(2))
+//    Console.WriteLine($"Key 2: {table[2]}");
+#endregion
 #endregion
