6aefee0e68a17e2a96b219086ceda3a07dc2e21b
URLeeo <mammadzadaaslan05@gmail.com>
2025-10-29 20:02:20 +0400

    Added new task about delegate

--- C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Program.cs
+++ C# Introduction/CodeAcademy_Tasks/CodeAcademy_Tasks/Program.cs
@@ -540,3 +540,37 @@ using System.Security.Cryptography.X509Certificates;
 //    Console.WriteLine($"Key 2: {table[2]}");
 #endregion
 #endregion
+
+#region Delegate
+//class Program
+//{
+//    public delegate bool StringFilter(string str);
+
+//    public static List<string> FilterStrings(List<string> items, StringFilter filter)
+//    {
+//        List<string> result = new List<string>();
+
+//        foreach (var item in items)
+//        {
+//            if (filter(item))
+//            {
+//                result.Add(item);
+//            }
+//        }
+
+//        return result;
+//    }
+
+//    public static void Main()
+//    {
+//        var words = new List<string> { "apple", "banana", "cherry", "avocado" };
+
+//        var filtered = FilterStrings(words, s => s.StartsWith("a"));
+
+//        foreach (var word in filtered)
+//        {
+//            Console.WriteLine(word);
+//        }
+//    }
+//}
+#endregion
