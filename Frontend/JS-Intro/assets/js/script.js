c1184f9ddfe5c09ea3d06cc1a11e13eb9c65091b
URLeeo <mammadzadaaslan05@gmail.com>
2025-11-24 19:09:35 +0400

    Added simple tasks for js intro

---
+++ Frontend/JS-Intro/assets/js/script.js
@@ -0,0 +1,30 @@
+// let arr = [3, 5, 7, 2];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+
+// console.log("Sum:", sum);
+
+
+// let word = "hello";
+// let reversed = "";
+
+// for (let i = word.length - 1; i >= 0; i--) {
+//     reversed += word[i];
+// }
+
+// console.log(reversed);
+
+
+// let text = "javascript";
+// let count = 0;
+
+// for (let char of text) {
+//     if (char === "a") {
+//         count++;
+//     }
+// }
+
+// console.log(count);
