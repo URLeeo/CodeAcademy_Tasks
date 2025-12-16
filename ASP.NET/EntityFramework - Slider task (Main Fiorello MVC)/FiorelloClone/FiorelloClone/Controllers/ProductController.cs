2740eee6d39615553736db082ffea6147292104d
URLeeo <mammadzadaaslan05@gmail.com>
2025-12-17 00:22:42 +0400

    Updated Fiorello Clone according to the last task

---
+++ ASP.NET/EntityFramework - Slider task (Main Fiorello MVC)/FiorelloClone/FiorelloClone/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using FiorelloClone.Data;
+using FiorelloClone.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloClone.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly AppDbContext _context;
+    public ProductController(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        IEnumerable<Product> products = await _context.Products
+            .Include(m => m.ProductImages)
+            .Include(m => m.Category)
+            .Take(4)
+            .Skip(4)
+            .Where(m => !m.IsDeleted)
+            .ToListAsync();
+        return View(products);
+    }
+}
